f59993d596046e16455cf5ee9efd53d02ca8d3c2
Adrian Pottinger <adrianpottinger@sofit.ltd>
2019-01-04 12:26:57 +0500

    added heroku

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -1,45 +1,67 @@
 import React, { Component } from 'react';
 import ReactPlayer from 'react-player'
-// import {  NavItem, Nav, NavDropdown,MenuItem, Navbar, Image  } from 'react-bootstrap';
+import { Player, ControlBar } from 'video-react';
+import ReactJWPlayer from 'react-jw-player';
+// import { Button } from 'react-bootstrap';
 
 
-// import './footer.css'
+import './Home.css'
 
 class Home extends Component {
 
+  constructor()
+  {
+    super();
+
+
+    this.play = this.play.bind(this);
+
+  }
+
+  play =()=>
+  {
+    console.log("player played");
+    // this.refs.player.play();
+    this.refs.player1.play();
+    this.setState((state, props) => {
+      return {counter: 0 + props.step};
+    });
+  }
+
+  componentDidMount = () =>
+  {
+    console.log("did mount ::");
+    // this.refs.player.subscribeToStateChange(this.handleStateChange.bind(this));
+     // this.play();
+     this.refs.player1.play();
+     this.setState((state, props) => {
+       return {counter: 0 + props.step};
+     });
+   }
+
+
+
   render() {
+    console.log("render");
+
     return (
 
       <div style={{width:"100%", height:"400px",border:"1px solid", display:"flex", justifyContent:"center", padding:"100px 0px 100px 0px"}}>
 
-        <div style={{width:"30%", marginRight:"10px"}}>
-          <ReactPlayer url='https://www.youtube.com/watch?v=ysz5S6PUM-U'
-            playing={true}
-            volume
-            muted={true}
-            width="100%"
-            height="100%"
-          />
+        <div style={{width:"39%", marginRight:"10px", background:"red"}}>
+
+          <Player ref="player1"
+            src="https://s3.amazonaws.com/codecademy-content/courses/React/react_video-slow.mp4"
+          >
+            <ControlBar autoHide={true} />
+          </Player>
+
         </div>
 
-        <div style={{width:"30%"}}>
-          <ReactPlayer url='https://www.youtube.com/watch?v=ysz5S6PUM-U'
-            playing={true}
-            volume
-            muted={true}
-            width="100%"
-            height="100%"
-          />
-        </div>
+        <div style={{width:"39%", background:"red"}}>
 
-        <div style={{width:"30%", marginLeft:"10px"}}>
-          <ReactPlayer url='https://www.youtube.com/watch?v=ysz5S6PUM-U'
-            playing={true}
-            volume
-            muted={true}
-            width="100%"
-            height="100%"
-          />
+          <ReactPlayer url='https://www.youtube.com/watch?v=ysz5S6PUM-U' playing />
+
         </div>
 
       </div>
@@ -49,3 +71,25 @@ class Home extends Component {
 }
 
 export default Home;
+
+
+// <div style={{width:"30%"}}>
+//   <ReactPlayer url='https://www.youtube.com/watch?v=H3p99E4Hw9Q&list=RDH3p99E4Hw9Q&start_radio=1'
+//
+//     playing={true}
+//     volume
+//     muted={true}
+//     width="100%"
+//     height="100%"
+//   />
+// </div>
+//
+// <div style={{width:"30%", marginLeft:"10px"}}>
+//   <ReactPlayer url='https://www.youtube.com/watch?v=ysz5S6PUM-U'
+//     playing={true}
+//     volume
+//     muted={true}
+//     width="100%"
+//     height="100%"
+//   />
+// </div>
--- server.js
+++ server.js
@@ -9,3 +9,12 @@ app.listen(port, () => console.log(`Listening on port ${port}`));
 app.get('/express_backend', (req, res) => {
   res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
 });
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
