996afe7cf540b7e6dfab31ea60acb9a889e522eb
Sheeraz Anwar <sheeraz.anwar@outlook.com>
2019-01-09 19:58:32 +0500

    combine ticker issue closed

--- api/video.js
+++ api/video.js
@@ -3,6 +3,7 @@ const gm = require('gm');
 const width = 1000;
 const height = 100;
 var fs = require('fs')
+var path = require('path')
 const ffmpegInstaller = require('@ffmpeg-installer/ffmpeg');
 var ffmpeg = require('fluent-ffmpeg');
 var command = ffmpeg();
@@ -26,23 +27,25 @@ exports.combineTickers = function(req,res){
   params.screenshots = params.screenshots.sort();
   if(params.screenshots!=null && params.screenshots!=undefined && params.screenshots.length>0){
     var headline = [];
-    for(var k=params.screenshots.length-1 ; k>0 ; k--){
+    for(var k=params.screenshots.length-1 ; k>=0 ; k--){
       if(start == 500){
          x = gm()
       }
+      console.log((__dirname).toString().replace('/api',""))
       x.in('-page', '+0+'+(start).toString())  // Custom place for each of the images
-      .in(path.join(__dirname)+parms.screenshots[k])
+      .in(path.join((__dirname).toString().replace('/api',""))+params.screenshots[k])
       start = start-50;
+      console.log(k)
       if(k ==0 || start-50<0){
         x.minify()  // Halves the size, 512x512 -> 256x256
         x.mosaic()  // Merges the images as a matrix
-        var dir = path.join(__dirname)+'/headlines/'+new Date().getYear().toString()+new Date().getMonth()+new Date().getTime().toString();
+        var dir = path.join((__dirname).toString().replace('/api',""))+'/headlines/';
         if (!fs.existsSync(dir)){
               fs.mkdirSync(dir);
           }
         x.write(dir+'/output'+count+'.jpg', function (err) {
             if (err) console.log(err);
-            res.sendFile(dir+'/output'+count+'.jpg');
+            res.status(200).send({image:'/output'+count+'.jpg'});
         });
         count = count+1;
         start = 500;
