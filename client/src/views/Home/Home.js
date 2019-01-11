b6c69330fd0ae6e7c52d6858b0ad037243c1fc2a
Adrian Pottinger <adrianpottinger@sofit.ltd>
2019-01-11 16:39:39 +0500

    added transcript page

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import ReactPlayer from 'react-player'
 // import { Player, ControlBar } from 'video-react';
 // import ReactJWPlayer from 'react-jw-player';
-// import { Button } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 import Screenshots from'./Screenshots.js'
+import Transcript from'./Transcript.js'
 import './Home.css'
 
 var request = require("request");
-// const url = "http://localhost:5000/"
-const url = "https://mts-prototype.herokuapp.com/"
+const url = "http://localhost:5000/"
+//const url = "https://mts-prototype.herokuapp.com/"
 
 class Home extends Component {
 
@@ -25,12 +26,12 @@ class Home extends Component {
     <div className="HomeStyle">
 
       <div className="videoGrid" >
-        <div style={{height:"10%"}}>
+        <div>
           <p className="videoTitle">Ary news</p>
         </div>
         <div className="videoStyle" onClick={this.handleVideo}>
           <ReactPlayer
-            width="100%"
+            width="99.9%"
             height="100%"
             url={url+"uploads/videoplayback.mp4"}
             playing
@@ -39,14 +40,25 @@ class Home extends Component {
             muted
             />
         </div>
-        <div style={{height:"10%"}}>
-          <p style={{fontWeight:"bold"}}>Click on video to view sorted News Tickers</p>
+        <div>
+          <p className="videoClickDescription">Click on video to view sorted News Tickers</p>
+          <Button bsStyle="primary" onClick={this.handleTranscript}>View Transcripts</Button>
         </div>
       </div>
 
 
 
     </div>
+     this.setState((state, props) => {
+       return {counter: 0 + props.step};
+     });
+   }
+
+   handleTranscript=()=>
+   {
+     console.log("transcript button clicked ::");
+     this.HomeContent=<Transcript />
+
      this.setState((state, props) => {
        return {counter: 0 + props.step};
      });
